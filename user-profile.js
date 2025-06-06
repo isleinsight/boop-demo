5130259178241ee2861078af6693a49bd7b83626
isleinsight <davon.isleinsight@gmail.com>
2025-06-06 10:34:48 +0000

    Update user-profile.js

--- user-profile.js
+++ user-profile.js
@@ -68,6 +68,11 @@ let lastVisibleStudent = null;
 let allStudentDocs = [];
 let searchMode = false;
 
+let filteredUsers = [];
+let currentPage = 1;
+const rowsPerPage = 5;
+let currentSort = { column: null, direction: "asc" };
+
 onAuthStateChanged(auth, (user) => {
   if (user) {
     loadUserProfile(uid);
