e057b369318b21d6c979c3eb49985363f597e8c8
isleinsight <davon.isleinsight@gmail.com>
2025-06-29 17:39:28 +0000

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,14 @@
+// backend/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Required for DigitalOcean managed PostgreSQL
-  }
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
