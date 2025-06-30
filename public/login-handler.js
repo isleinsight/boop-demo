40bd7646adac43c290ebde7aace29ce6de0dc127
isleinsight <davon.isleinsight@gmail.com>
2025-06-30 09:54:35 -0300

    Update login-handler.js

--- public/login-handler.js
+++ public/login-handler.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
   const password = document.getElementById('password').value;
 
   try {
-    const res = await fetch('/api/login', {
+    const res = await fetch('/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
