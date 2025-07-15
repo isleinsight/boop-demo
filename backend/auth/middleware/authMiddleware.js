1b62eb91efb9bb219c080618a99b1418d0c84830
isleinsight <davon.isleinsight@gmail.com>
2025-07-15 18:28:36 -0300

    Update authMiddleware.js

--- backend/auth/middleware/authMiddleware.js
+++ backend/auth/middleware/authMiddleware.js
@@ -1,14 +1,17 @@
-// backend/auth/middleware/auth-middleware.js
 const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; // "Bearer TOKEN"
+  const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.sendStatus(401);
+  if (!token) {
+    return res.status(401).json({ message: "Token missing" });
+  }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
     req.user = user;
     next();
   });
