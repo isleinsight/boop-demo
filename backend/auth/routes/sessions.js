2ad286aafc3fec30b150597a045b2ee6451d0aa1
isleinsight <davon.isleinsight@gmail.com>
2025-07-19 17:51:27 -0300

    Update sessions.js

--- backend/auth/routes/sessions.js
+++ backend/auth/routes/sessions.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../../db');
-const authenticateToken = require('../middleware/authMiddleware');
+
 
 router.post('/', async (req, res) => {
   const { email, status } = req.body;
