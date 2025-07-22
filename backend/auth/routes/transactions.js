c1c207d206aa2ed3ae214d067feca4a49019dbdb
isleinsight <davon.isleinsight@gmail.com>
2025-07-22 15:38:52 -0300

    Update transactions.js

--- backend/auth/routes/transactions.js
+++ backend/auth/routes/transactions.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../db");
+const pool = require("../../db");
 const authenticateToken = require("../middleware/authMiddleware");
 
 // ✅ Get all transactions (with filters & pagination)
