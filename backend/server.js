51ae19b7b6e5d1ccb15592b2785ad30d91d245a7
isleinsight <davon.isleinsight@gmail.com>
2025-07-28 16:22:50 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,10 @@ try {
   app.use('/api/users', require('./auth/routes/users'));
   app.use('/api/cards', require('./auth/routes/cards'));
   app.use('/api/wallets', require('./auth/routes/wallets'));
-  // app.use('/api/vendors', require('./auth/routes/vendors'));
+  app.use('/api/vendors', require('./auth/routes/vendors'));
   app.use('/api/user-students', require('./auth/routes/userStudents'));
   app.use('/api/sessions', require('./auth/routes/sessions'));
-  app.use('/api/transactions', require('./auth/routes/transactions'));
+  // app.use('/api/transactions', require('./auth/routes/transactions'));
   app.use('/api/treasury', require('./auth/routes/treasury'));
   app.use('/api/admin-actions', require('./auth/routes/admin-actions'));
 } catch (err) {
