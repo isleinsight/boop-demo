a29cbe66c2fc5807a1501e144c3e6fd84cf7a154
isleinsight <davon.isleinsight@gmail.com>
2025-07-31 06:32:45 -0300

    Update user-profile.js

--- public/admin/user-profile.js
+++ public/admin/user-profile.js
@@ -307,9 +307,7 @@ if (user.role === "student") {
       expiry_date: document.getElementById("editExpiry")?.value,
     };
 console.log("🚨 PATCHing to:", `/api/students/${currentUserId}`);
-    console.log("PATCHING student with user_id:", id);
-console.log("Final SQL:", updateQuery);
-console.log("Values:", values);
+    
     try {
       await fetchJSON(`/api/students/${currentUserId}`, {
         
