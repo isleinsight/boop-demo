db429b2974793e1ed6ecd75ab89d9904fd018a75
isleinsight <davon.isleinsight@gmail.com>
2025-07-31 14:57:00 -0300

    Update users.js

--- backend/auth/routes/users.js
+++ backend/auth/routes/users.js
@@ -9,7 +9,7 @@ const {
 
 const logAdminAction = require("../middleware/log-admin-action");
 
-const rolesWithWallet = ["cardholder", "student", "senior", "vendor"];
+const rolesWithWallet = ["cardholder", "student", "senior", "vendor", "parent"];
 const isValidUUID = (str) =>
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(str);
 
