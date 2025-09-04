810d65f36978d5ed4c2e509a4f8b5138773a6398
isleinsight <davon.isleinsight@gmail.com>
2025-09-04 16:15:54 -0300

    Create vendor-common.js

---
+++ public/vendor/vendor-common.js
@@ -0,0 +1,31 @@
+<script>
+/**
+ * Vendor-safe fetch wrapper:
+ * - Adds Authorization header from localStorage('boop_jwt')
+ * - If server sends a renewed token in `x-renew-jwt`, we save it
+ */
+async function apiFetch(url, opts = {}) {
+  const token = localStorage.getItem('boop_jwt');
+  const headers = { 'Content-Type': 'application/json', ...(opts.headers || {}) };
+  if (token) headers.Authorization = `Bearer ${token}`;
+
+  const res = await fetch(url, { ...opts, headers });
+
+  const renewed = res.headers.get('x-renew-jwt');
+  if (renewed) localStorage.setItem('boop_jwt', renewed);
+
+  return res;
+}
+
+// Optional helpers if you like concise code:
+async function apiJSON(url, opts = {}) {
+  const res = await apiFetch(url, opts);
+  const data = await res.json().catch(()=> ({}));
+  if (!res.ok) throw new Error(data.message || data.error || `HTTP ${res.status}`);
+  return data;
+}
+function apiGet(url){ return apiJSON(url); }
+function apiPost(url, body){ return apiJSON(url, { method:'POST', body: JSON.stringify(body||{}) }); }
+function apiPatch(url, body){ return apiJSON(url, { method:'PATCH', body: JSON.stringify(body||{}) }); }
+function apiDelete(url, body){ return apiJSON(url, { method:'DELETE', body: JSON.stringify(body||{}) }); }
+</script>
