ed4c1a4c377b2b4fdf71256289254a58ce0c7971
isleinsight <davon.isleinsight@gmail.com>
2025-09-11 11:17:37 -0300

    Create app.config.js

---
+++ mobile/vendor/app.config.js
@@ -0,0 +1,11 @@
+export default {
+  name: "Payulot Vendor",
+  slug: "payulot-vendor",
+  scheme: "payulotvendor",
+  version: "1.0.0",
+  ios: { bundleIdentifier: "com.payulot.vendor" },
+  android: { package: "com.payulot.vendor" },
+  extra: {
+    API_BASE: "https://payulot.com"
+  }
+};
