4fb54de0ce35d21e6215dafa37f33a694bb08e9c
isleinsight <davon.isleinsight@gmail.com>
2025-09-11 11:00:00 -0300

    Update app.config.js

--- mobile/cardholder/app.config.js
+++ mobile/cardholder/app.config.js
@@ -1,1 +1,11 @@
-
+export default {
+  name: "Payulot Cardholder",
+  slug: "payulot-cardholder",
+  scheme: "payulotcard",
+  version: "1.0.0",
+  ios: { bundleIdentifier: "com.payulot.cardholder" },
+  android: { package: "com.payulot.cardholder" },
+  extra: {
+    API_BASE: "https://payulot.com"
+  }
+};
