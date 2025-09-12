c29006d100f19e067522e2626af89f5582dd027c
isleinsight <davon.isleinsight@gmail.com>
2025-09-12 13:24:41 -0300

    Update App.js

--- mobile/cardholder/App.js
+++ mobile/cardholder/App.js
@@ -1,7 +1,6 @@
 // App.js
 import 'react-native-gesture-handler';
 import 'react-native-reanimated';
-import 'react-native-url-polyfill/auto';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
