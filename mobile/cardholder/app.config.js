6867a01d62fb5e696ccefb986d9a22aaf165b2d5
isleinsight <davon.isleinsight@gmail.com>
2025-09-12 12:07:55 -0300

    Update app.config.js

--- mobile/cardholder/app.config.js
+++ mobile/cardholder/app.config.js
@@ -1,11 +1,11 @@
 export default {
-  name: "cardholder",
-  slug: "cardholder",
-  version: "1.0.0",
-  extra: {
-    API_BASE: "https://payulot.com"
-  },
-  plugins: [
-    "expo-asset"
-  ]
-};
+  expo: {
+    name: "cardholder",
+    slug: "cardholder",
+    version: "1.0.0",
+    extra: {
+      API_BASE: "https://payulot.com"
+    }
+
+  }
+}
