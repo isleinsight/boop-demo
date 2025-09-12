e62310a5b48fab3b8dfc92467fea641e215ad246
isleinsight <davon.isleinsight@gmail.com>
2025-09-11 21:57:12 -0300

    Update babel.config.js

--- mobile/cardholder/babel.config.js
+++ mobile/cardholder/babel.config.js
@@ -2,7 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // keep this if you add Reanimated later; harmless if unused
     plugins: ['react-native-reanimated/plugin'],
   };
 };
